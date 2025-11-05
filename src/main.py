cb866d47b209b0e99f5351ce5ba92ffb828276aa
FPAULMV <fpaulmv1395@gmail.com>
2025-11-05 17:34:00 -0600

    Avances a la fecha

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-import versioning, os, urllib.parse, sys
+import versioning, os, urllib.parse, sys, pandas
 from pathlib import Path
 from sqlalchemy import create_engine, text
 from pandas import DataFrame
@@ -6,38 +6,61 @@ from dotenv import load_dotenv
 
 
 
-
-try:
-    load_dotenv()
-    PATH_FILES_SINERGIA = os.getenv('PATH_FILES_SINERGIA')
-    PATH_FILES_PETRODIESEL = os.getenv('PATH_FILES_PETRODIESEL')
-    PATH_FILES_PROENERGETICS = os.getenv('PATH_FILES_PROENERGETICS')
-    CONN_STR = os.getenv('CONN_STR')
-    odbc_encoded = urllib.parse.quote_plus(CONN_STR)
-    engine = create_engine(f"mssql+pyodbc:///?odbc_connect={odbc_encoded}",pool_pre_ping=True,fast_executemany=True)
-    print("\n--- Variables de entorno cargadas con exito. ---\n")
-    
-except Exception as e:
-    # Agregar Log.Error
-    print("Hubo un error al cargar las variables de entorno.")
-    print(e)
-    sys.exit("-> Fin de la ejecucion del programa. <-".upper())
-finally:
-    #Agregar Log.info
-    print("-> FIN: de la carga de las variables de entorno.\n")
-
-
-
 class Vencimientos():
-    def __init__(self, query: str, ruta_documentos: Path):
-        self.consulta_vencimientos = text(query)
-        self.ruta_documentos = Path(ruta_documentos)
+    def __init__(self, query: str, output_documentos: Path):
+        self.__QUERY_VENCIMIENTOS = text(query)
+        self.__OUTPUT_DOCUMENTOS = Path(output_documentos)
+        self.__CAMPOS_REQUERIDOS = [
+            "Destino",
+            "NumeroFactura",
+            "FechaFactura",
+            "Vencimiento",
+            "Total",
+            "Saldo",
+            "ReferenciaBancaria",
+            "DiasCredito", 
+            "DiasVencido"
+        ]
+        self.DF_VENCIMIENTOS =  self.obtener_vencimientos(self.__QUERY_VENCIMIENTOS)
+        self.validar_campos(self.DF_VENCIMIENTOS, self.__CAMPOS_REQUERIDOS)
+        
 
-    def vencimientos(self):
-        pass
+
+    def obtener_vencimientos(self, query: str) -> DataFrame:
+        try:
+            with engine.connect() as conn:
+                df = pandas.read_sql(query, conn)
+                if df.empty:
+                    print("La consulta devolvio un DataFrame vacio.")
+                    sys.exit("-> Fin de la ejecucion del programa. <-".upper())
+                    
+                return df
+        except Exception as e:
+            print(e)
+        finally:
+            print("-> FIN: Termino la consulta de los vencimientos.\n")
+
+    def validar_campos(self, data_frame: DataFrame, campos: list) -> bool:
+        try:
+            value = False
+            campos_formateados = {str(c).upper().strip().replace(" ", "").replace("_", "") for c in campos}
+            df_columnas = {str(c).upper().strip().replace(" ", "").replace("_", "") for c in data_frame.columns}
+            faltantes =  campos_formateados - df_columnas
+            if faltantes:
+                sys.exit(f"Faltan columnas en el DataFrame {sorted(faltantes)}")
+
+            value = True
+            return value
+        except Exception as e:
+            print("ERROR: Ocurrio un error al comprobar los campos del DataFrame")
+            print(e)
+        finally:
+            print("-> FIN: Termino la validacion de las columnas del DataFrame.\n")
 
 
-    def clientes(self):
+    def clientes(self, data_frame: DataFrame) -> set:
+        clientes = data_frame["Destinos"]
+        
         pass
     
 
@@ -55,3 +78,38 @@ class Vencimientos():
 
     def exec_insert(self):
         pass
+
+
+
+
+try:
+    load_dotenv()
+    PATH_FILES_SINERGIA = os.getenv('PATH_FILES_SINERGIA')
+    QUERY_SINERGIA = os.getenv('QUERY_SINERGIA')
+
+    PATH_FILES_PETRODIESEL = os.getenv('PATH_FILES_PETRODIESEL')
+
+    PATH_FILES_PROENERGETICS = os.getenv('PATH_FILES_PROENERGETICS')
+
+    CONN_STR = os.getenv('CONN_STR')
+    odbc_encoded = urllib.parse.quote_plus(CONN_STR)
+    engine = create_engine(f"mssql+pyodbc:///?odbc_connect={odbc_encoded}",pool_pre_ping=True,fast_executemany=True)
+    print("\n--- Variables de entorno cargadas con exito. ---\n")
+    
+except Exception as e:
+    # Agregar Log.Error
+    print("Hubo un error al cargar las variables de entorno.")
+    print(e)
+    sys.exit("-> Fin de la ejecucion del programa. <-".upper())
+finally:
+    #Agregar Log.info
+    print("-> FIN: de la carga de las variables de entorno.\n")
+
+
+
+
+if __name__ == '__main__':
+    
+    sinergia = Vencimientos(QUERY_SINERGIA, PATH_FILES_SINERGIA)
+    print(sinergia.MIS_VENCIMIENTOS)
+    sinergia
