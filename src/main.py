b13100ac19b6ef8610160c238859a9234cf75a17
FPAULMV <fpaulmv1395@gmail.com>
2025-11-07 17:44:30 -0600

    Actualizacion del avance:
    Hasta el momento:
    - Consulta los vencimientos.
    - Obtiene los clientes de la consulta de vencimientos.
    - Obtiene los id portal de c/cliente.
    Tambien construimos el nombre del cliente, el nombre del archivo y
    dividimos el DataFrame en secciones con la informacion unica de c/cliente.
    - Creamos el documento .csv con la informacion de cada cliente.
    
    Mejoras:
    
    Lo anterior esta escrito fuera de un objeto así que tendriamos que reescribirlo para
    cada cliente. Hay que integrar esto como metodos dentro de una clase para utilizarlos
    en distintos objetos.
    
    Falta:
    
    Mover el conjunto de todos los documentos creados al servidor 50.
    Agregar logs.
    Agregar un objeto que se encargue de enviar los documentos creados
    por correo para los clientes que resiven sus documentos por correo.

--- src/main.py
+++ src/main.py
@@ -7,28 +7,12 @@ from dotenv import load_dotenv
 
 
 class Vencimientos():
-    def __init__(self, query: str, output_documentos: Path):
-        self.__QUERY_VENCIMIENTOS = text(query)
-        self.__OUTPUT_DOCUMENTOS = Path(output_documentos)
-        self.__CAMPOS_REQUERIDOS = [
-            "Destino",
-            "NumeroFactura",
-            "FechaFactura",
-            "Vencimiento",
-            "Total",
-            "Saldo",
-            "ReferenciaBancaria",
-            "DiasCredito", 
-            "DiasVencido"
-        ]
-        self.DF_VENCIMIENTOS =  self.obtener_vencimientos(self.__QUERY_VENCIMIENTOS)
-        self.validar_campos(self.DF_VENCIMIENTOS, self.__CAMPOS_REQUERIDOS)
-        self.LISTA_CLIENTES = self.clientes(self.DF_VENCIMIENTOS)
-        
-
+    def __init__(self):
+        pass
 
     def obtener_vencimientos(self, query: str) -> DataFrame:
         try:
+            query = text(query)
             with engine.connect() as conn:
                 df = pandas.read_sql(query, conn)
                 if df.empty:
@@ -37,22 +21,37 @@ class Vencimientos():
                     
                 return df
         except Exception as e:
-            print(e)
+            print("ERROR: Hubo un error al consultar los vencimientos.")
+            print(f"DETALLE: {e}")
+            sys.exit("-> Fin de la ejecucion del programa. <-".upper())
         finally:
             print("-> FIN: Termino la consulta de los vencimientos.\n")
 
-    def validar_campos(self, data_frame: DataFrame, campos: list) -> bool:
+    def comprobar_columnas(self, data_frame: DataFrame, columnas: list = None) -> DataFrame:
+        """
+            Comprueba que un DataFrame contenga todas las columnas nombradas
+            en una lista.
+        """
+        if columnas is None:
+            columnas = ["Destino", "NumeroFactura", "FechaFactura",
+                        "Vencimiento", "Total", "Saldo",
+                        "ReferenciaBancaria", "DiasCredito", "DiasVencido",
+                        "ClientId"]
+
         try:
-            value = False
-            campos_formateados = {str(c).upper().strip().replace(" ", "").replace("_", "") for c in campos}
+            campos_formateados = {str(c).upper().strip().replace(" ", "").replace("_", "") for c in columnas}
             df_columnas = {str(c).upper().strip().replace(" ", "").replace("_", "") for c in data_frame.columns}
             faltantes =  campos_formateados - df_columnas
             if faltantes:
                 print(f"Faltan columnas en el DataFrame {sorted(faltantes)}")
                 sys.exit("-> Fin de la ejecucion del programa. <-".upper())
 
-            value = True
-            return value
+            df_filtrado = data_frame[['Destino', 'NumeroFactura', 'FechaFactura', 
+                                      'Vencimiento', 'total', 'Saldo', 
+                                      'ReferenciaBancaria', 'dias_credito', 'dias_vencido',
+                                      'client_id']]
+            
+            return df_filtrado
         except Exception as e:
             print("ERROR: Ocurrio un error al comprobar los campos del DataFrame")
             print(e)
@@ -60,10 +59,14 @@ class Vencimientos():
             print("-> FIN: Termino la validacion de las columnas del DataFrame.\n")
 
 
-    def clientes(self, data_frame: DataFrame) -> list:
+    def clientes(self, data_frame: DataFrame) -> DataFrame:
+        """
+            Obtiene una lista con los clientes del DataFrame. 
+            (O de lo que sea que incluya la columna 'Destinos.')
+        """
         try:    
-            clientes = data_frame["Destinos"].dropna().unique().tolist()
-            if not clientes:
+            clientes = data_frame[["Destino", "client_id"]].dropna().drop_duplicates(subset="Destino")
+            if clientes.empty:
                 print("INFO: No hay clientes para validar en el DataFrame.")
                 sys.exit("-> Fin de la ejecucion del programa. <-".upper())
             return clientes
@@ -74,13 +77,31 @@ class Vencimientos():
         finally:
             print("-> FIN: Termino de obtener la lista de los clientes unicos.\n")
 
-    def portal_id_clientes(self):
-        
-        pass
 
 
-    def crear_documentos(self):
-        pass
+
+
+
+class CrearDoumento():
+    """Crea documentos a partir de un DataFrame."""
+
+    def __init__(self, data_frame: DataFrame, carpeta_salida: Path):
+        self.data_frame = data_frame
+        self.carpeta_salida = Path(carpeta_salida)
+
+
+    def crear_documentos_csv(self, nombre_archivo: str) -> None:
+        """ Crea un documento '.csv' """
+        try:
+            ruta = Path(self.carpeta_salida / nombre_archivo)
+            print(f"RUTA SALIDA: {ruta}")
+            self.data_frame.to_csv(ruta, index= False, encoding= 'utf-8')
+            return ruta
+        except Exception as e:
+            print(f"Hubo un error al crear el archivo. {ruta}")
+            print(e)
+        finally:
+            (print("-> FIN: Termino la creacion del archivo."))
 
 
     def cardsystem_insert(self):
@@ -106,6 +127,7 @@ try:
     odbc_encoded = urllib.parse.quote_plus(CONN_STR)
     engine = create_engine(f"mssql+pyodbc:///?odbc_connect={odbc_encoded}",pool_pre_ping=True,fast_executemany=True)
     print("\n--- Variables de entorno cargadas con exito. ---\n")
+
     
 except Exception as e:
     # Agregar Log.Error
@@ -121,6 +143,42 @@ finally:
 
 if __name__ == '__main__':
     
-    sinergia = Vencimientos(QUERY_SINERGIA, PATH_FILES_SINERGIA)
-    print(sinergia.MIS_VENCIMIENTOS)
-    sinergia
+    sinergia = Vencimientos()
+    df_completo = sinergia.obtener_vencimientos(QUERY_SINERGIA)
+    df_vencimientos = sinergia.comprobar_columnas(df_completo)
+    clientes = sinergia.clientes(df_vencimientos)
+    
+    CLIENTES_NO_ENCONTRADOS = []
+    INSERTS_ARCHIVOS_GENERALES = []
+
+    for _, row in clientes.iterrows():
+        cliente = row['Destino']
+        if cliente in df_vencimientos['Destino'].values:
+            df_cliente = df_vencimientos[df_vencimientos['Destino'] == cliente]
+            nombre_cliente = str(cliente)
+            nombre_archivo = f"Vencimientos {nombre_cliente}.csv"
+            client_id = df_cliente['client_id'].unique()
+            
+            df_cliente_a_mostrar = df_cliente[['Destino', 'NumeroFactura', 'FechaFactura', 
+                                      'Vencimiento', 'total', 'Saldo', 
+                                      'ReferenciaBancaria', 'dias_credito', 'dias_vencido']]
+
+            archivo_salida_sinergia = CrearDoumento(df_cliente_a_mostrar, PATH_FILES_SINERGIA)
+            archivo_salida_sinergia.crear_documentos_csv(nombre_archivo)
+
+            # Me falta agregar una funcion que alimente las lista de insert a la cardsystem.archivosgenerales.
+            query = "INSERT INTO [NexusFuel].[CardSystem].[ArchivosGenerales]"
+            
+            with engine.connect() as conn:
+
+                conn.execute()
+
+            
+        else:
+            CLIENTES_NO_ENCONTRADOS.append(cliente)
+
+
+
+
+
+    print(("-> Fin de la ejecucion del programa. <-".upper()))
